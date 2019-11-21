bb52c9224d8588903febce7c14dfd6d537ac2537
Thys Meintjes <matthys.meintjes@bhp.com>
2019-11-21 12:34:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# TPUParty
+
+A set of tools and toys to work with the Google TPU
+
+At this time tensorflow is not yet available for Python 3.8, so use the next best thing
+
+```zsh
+$ mkvirtualenv -p python3.7 tpuparty 
+```
---
+++ setup.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+from glob import glob
+from os.path import basename, splitext
+
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='TPUParty',
+    license='MIT',
+    version='0.0.1',
+    description='Tools and toys to use with the Coral TPU',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'tpuparty=tpuparty.cli:cli',
+        ],
+    },
+    install_requires=[
+        'python-opencv',
+        'tensorflow',
+    ],
+    url='',
+    classifiers=[
+        'License :: MIT',
+        'Development Status :: 4 - Beta',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+    keywords=[],
+    extras_require={},
+    setup_requires=[],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    package_data={
+        '': ['config/*.yml'],
+    },
+)
